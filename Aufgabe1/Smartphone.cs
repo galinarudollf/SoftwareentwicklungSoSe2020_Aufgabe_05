3d9e4a8dbcbd3b5a8ebf5b9bee714ac7e1d0ec47
Galina Rudolf <galina.rudolf@gmail.com>
2020-06-16 10:35:50 +0200

    plus Aufgabe 1 und Klassendiagramm fuer Aufgabe 2

---
+++ Aufgabe1/Smartphone.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Aufgabe1
+{
+
+    class Smartphone
+    {
+        private int? pin=null;
+        private int versuche=0;
+        private bool authentifizieren()
+        {
+            if (versuche == 3)
+            {
+                Console.WriteLine("Authentifizierungsfehler");
+                return false;
+            }
+            if (pin == null) return true;
+            Console.WriteLine("Pin?");
+            try
+            { 
+                do
+                {
+                    if (Int32.Parse(Console.ReadLine()) == pin)
+                    {
+                        versuche = 0;
+                        return true;
+                    }
+                    else versuche++;
+                } while (versuche < 3);
+                Console.WriteLine("Authentifizierungsfehler");
+                return false;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+        public void PinAendern()
+        {
+            if (authentifizieren()==true)
+            { 
+                Console.WriteLine("neue Pin?");
+                try
+                {
+                    string neu=Console.ReadLine();
+                    if (neu.Equals(string.Empty))
+                    {
+                        pin = null;
+                    }
+                    else pin=Int32.Parse(neu);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Smartphone phone = new Smartphone();
+            phone.PinAendern();
+            phone.PinAendern();
+        }
+    }
+}
